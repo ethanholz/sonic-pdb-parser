f3deb514226fc0ac21449dba4bf9522e9e64d2ae
Ethan Holz <ethan.holz@thoriumworks.com>
2023-11-19 18:19:27 -0500

    feat: added module to build.zig

--- build.zig
+++ build.zig
@@ -24,6 +24,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    b.addModule("sonic", .{ .source_file = .{ .path = "src/records.zig" } });
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
